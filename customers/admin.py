0d37bcfe4743908d15e6371ffaa04c26a05074a3
amahadnezhad <amahadnezhad@gmail.com>
2024-03-01 15:38:16 +0330

    Remoce education Field From Customer Model

--- customers/admin.py
+++ customers/admin.py
@@ -6,7 +6,7 @@ from .models import Customer
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('firstname', 'lastname', 'nat_id', 'father_name', 'date_of_birth', 'phone_number', 'country', 'city'
-                    , 'gender', 'job', 'vip', 'datetime_created', 'datetime_modified')
+                    , 'gender', 'job', 'vip', 'datetime_created', )
 
     ordering = ('datetime_created',)
 
