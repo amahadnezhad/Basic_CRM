291aa643dece262f2b8d3a39a9d733ab62f4ea84
amahadnezhad <amahadnezhad@gmail.com>
2024-03-10 13:08:32 +0330

    Use Translation In customers/models.py

--- customers/models.py
+++ customers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 
 class Customer(models.Model):
@@ -9,17 +10,17 @@ class Customer(models.Model):
         ('3', 'Other'),
     )
 
-    firstname = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=100)
-    nat_id = models.CharField(max_length=10)
-    father_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField()
-    phone_number = models.CharField(max_length=12)
-    country = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=1)
-    job = models.CharField(max_length=100)
-    vip = models.BooleanField(default=False)
+    firstname = models.CharField(verbose_name=_("FirstName"), max_length=100)
+    lastname = models.CharField(verbose_name=_("LastName"), max_length=100)
+    nat_id = models.CharField(verbose_name=_("National_ID"), max_length=10)
+    father_name = models.CharField(verbose_name=_("National_ID"), max_length=100)
+    date_of_birth = models.DateField(verbose_name=_("Date of Birth"))
+    phone_number = models.CharField(verbose_name=_("Phone Number"), max_length=12)
+    country = models.CharField(verbose_name=_("Country"), max_length=50)
+    city = models.CharField(verbose_name=_("City"), max_length=50)
+    gender = models.CharField(verbose_name=_("Gender"), choices=GENDER_CHOICES, max_length=1)
+    job = models.CharField(verbose_name=_("Job"), max_length=100)
+    vip = models.BooleanField(verbose_name=_("VIP"), default=False)
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
